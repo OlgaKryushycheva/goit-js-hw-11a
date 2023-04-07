a8547f1eb1181df3874820a768070a766f10b18e
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-04-07 15:03:23 +0300

    подготовка

--- src/component_js/pikcture_api_service.js
+++ src/component_js/pikcture_api_service.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-const API_KEY = '35143991-d0922efd298f1f4b6c593d4ee';
+const API_KEY = 'key=35143991-d0922efd298f1f4b6c593d4ee';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export class FotoApi {
+export default class PictureApiService {
   constructor() {
     this.inputFild = '';
     this.page = 1;
     this.per_page = 40;
   }
 
-  async fetchFotos() {
+  async fetchPictures() {
     try {
       const url = `${BASE_URL}?${API_KEY}&q=${this.inputFild}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.per_page}&page=${this.page}`;
 
--- src/index.js
+++ src/index.js
@@ -1,107 +1,100 @@
 import './css/styles.css';
+import PictureApiService from './component_js/pikcture_api_service'
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import { FotoApi } from './fetchFotos';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
+const gallery =  new SimpleLightbox('.gallery a', {
+         captionsData: 'alt',
 });
 
-const fotoApi = new FotoApi();
+const picturesApiService = new PictureApiService();
+        
+const refs = {
+    searchFormEl: document.querySelector('#search-form'),
+    cardContainer: document.querySelector('.gallery'),
+    loadMoBtnEl: document.querySelector('.load-more')
+};
 
-const searchFormEl = document.querySelector('#search-form');
-const galleryEl = document.querySelector('.gallery');
-const loadMorBtnEl = document.querySelector('.load-more');
+refs.searchFormEl.addEventListener('submit', hendleSearchSubmit);
+refs.loadMoBtnEl.addEventListener('click', hendleLoadMOClick);
 
-searchFormEl.addEventListener('submit', onSearchFormSubmit);
-loadMorBtnEl.addEventListener('click', onLoadMorBtn);
+const per_page = picturesApiService.per_page;
 
-const per_page = fotoApi.per_page;
+function hendleSearchSubmit(e) {
+    e.preventDefault();
+    picturesApiService.value = e.target.elements.searchQuery.value.trim();
+    loadMorButtonDisable()
 
-function onSearchFormSubmit(e) {
-  e.preventDefault();
-  fotoApi.value = e.target.elements.searchQuery.value.trim();
-  loadMorBtnDisable();
-
-  if (fotoApi.value === '') {
-    Notiflix.Notify.warning('Please enter, what exactly you want to find?');
-    return;
-  }
-  fotoApi.resetPage();
-  clearMarkup();
-  fotoApi.fetchFotos().then(({ hits, totalHits }) => {
-    if (hits.length === 0) {
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-      return;
+    if (picturesApiService.value === '') {
+        Notiflix.Notify.warning('Please enter, what exactly you want to find?');
+        return;
     }
+    picturesApiService.resetPage();
+    clearGalery();
+    picturesApiService.fetchPictures().then(({ hits, totalHits }) => {
+        
+        if (hits.length === 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            return;
+        }
+        
 
-    randerMarkup(hits);
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-
-    if (totalHits < per_page) {
-      Notiflix.Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-      return;
-    }
-    gallery.refresh();
-    loadMorBtnEnable();
-  });
+        renderMarkUp(hits)
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
+        
+    
+        if (totalHits < per_page) {
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+            return;
+        }
+        gallery.refresh();
+        loadMorButtonEnable();
+    })
 }
 
-function onLoadMorBtn() {
-  loadMorBtnDisable();
-  fotoApi.fetchFotos().then(card => {
-    randerMarkup(card.hits);
+function hendleLoadMOClick() {
 
-    const { height: cardHeight } =
-      galleryEl.firstElementChild.getBoundingClientRect();
+    loadMorButtonDisable()
+    picturesApiService.fetchPictures().then(card => {
+        renderMarkUp(card.hits);
 
-    window.scrollBy({
-      top: cardHeight * 2,
-      behavior: 'smooth',
-    });
+        const { height: cardHeight } = refs.cardContainer.firstElementChild.getBoundingClientRect();
+        
+        window.scrollBy({
+            top: cardHeight * 2,
+            behavior: "smooth",
+        });
 
-    loadMorBtnEnable();
-    if (card.hits.length < per_page) {
-      loadMorButtonDisable();
-      Notiflix.Notify.failure(
-        "We're sorry, but you've reached the end of search results."
-      );
-    }
-    gallery.refresh();
-  });
-}
 
-function randerMarkup(data) {
-  const markUp = createMarkup(data);
-  galleryEl.insertAdjacentHTML('beforeend', markUp);
-}
+        loadMorButtonEnable();
+        if (card.hits.length < per_page) {
+            loadMorButtonDisable()
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        }
+        gallery.refresh();
+    })
+};
 
-function clearMarkup() {
-  galleryEl.innerHTML = '';
-}
+function renderMarkUp(data) {   
+const markUp = createMarkUp(data)
+refs.cardContainer.insertAdjacentHTML('beforeend', markUp)
+};
 
-function createMarkup(array) {
-  return array
-    .map(foto => {
-      const {
-        tags,
-        webformatURL,
-        likes,
-        views,
-        comments,
-        downloads,
-        largeImageURL,
-      } = foto;
-      return `<div class="photo-card">
-       <a href="${largeImageURL}" class="gallery__item">
-       <img src="${webformatURL}" 
+function clearGalery() {
+    refs.cardContainer.innerHTML = "";
+};
+
+function createMarkUp(array) {
+    return array.map((picture) => {
+        const{tags, webformatURL, likes, views, comments, downloads, largeImageURL} = picture
+        return `<div class="photo-card">
+       <a href="${largeImageURL}" class="link-img">
+       <img class="gallery__image"
+       src="${webformatURL}" 
        alt="${tags}" 
+       width="340px" 
+       height="220px" 
        loading="lazy" />
        </a>
      <div class="info">
@@ -118,15 +111,14 @@ function createMarkup(array) {
            <b>Downloads <br />${downloads}</b>
          </p>
        </div>
-     </div>`;
-    })
-    .join('');
-}
+     </div>`
+    }).join("");
+};
 
-function loadMorBtnDisable() {
-  loadMorBtnEl.classList.add('hidden');
-}
+function loadMorButtonDisable() {
+    refs.loadMoBtnEl.classList.add('hidden')
+};
 
-function loadMorBtnEnable() {
-  loadMorBtnEl.classList.remove('hidden');
-}
+function loadMorButtonEnable() {
+   refs.loadMoBtnEl.classList.remove('hidden')
+};
